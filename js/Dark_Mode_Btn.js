aa20fe13f351aee3d6504bff96782437c0befe0c
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-07-25 12:52:55 +0200

    Fin resposnive

--- js/Dark_Mode_Btn.js
+++ js/Dark_Mode_Btn.js
@@ -3,12 +3,12 @@ const switchThemeBtn = document.querySelector('.changeTheme')
 
         switchThemeBtn.addEventListener('click', () => {
             if(toggleTheme === 0) {
-                document.documentElement.style.setProperty('--ecriture', '#f1f1f1');
-                document.documentElement.style.setProperty('--background', '#262626');
-                toggleTheme++;
-            } else {
                 document.documentElement.style.setProperty('--ecriture', '#262626');
                 document.documentElement.style.setProperty('--background', '#f1f1f1');
+                toggleTheme++;
+            } else {
+                document.documentElement.style.setProperty('--ecriture', '#f1f1f1');
+                document.documentElement.style.setProperty('--background', '#262626');
                 toggleTheme--;
             }
         })
